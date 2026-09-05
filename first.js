23edd364c584d9580ea4a426eafe235168731ad1
Noel Rego <20leonarego@gmail.com>
2026-09-05 14:08:30 +0530

    adding comments for better understanding

--- first.js
+++ first.js
@@ -1,5 +1,8 @@
 let readData=[];
 
+//getting the html nodes
+const timeslist=document.querySelectorAll("#daily,#weekly,#monthly");
+
 //getting data
 const loadData=async()=>{
     const response=await fetch("data.json");
@@ -19,9 +22,11 @@ const render=(timeframe)=>{
         const lastnewhrs=short.previous;
         console.log(boxId, dailynewhrs, lastnewhrs);
 
+        //making sure the id in html is same as the value in the data.json file
         const hoursEl = document.querySelector(`#${boxId}hrs`);
         const lastHoursEl = document.querySelector(`#last${boxId}hrs`); 
 
+        //updating if the variable exists
         if (hoursEl){
             hoursEl.innerText=`${dailynewhrs} hrs`;
         }
@@ -31,16 +36,7 @@ const render=(timeframe)=>{
     });
 };
 
-
-const previousLabel = (timeframe) => {
-    if (timeframe === "daily") return "Yesterday";
-    if (timeframe === "weekly") return "Last Week";
-    return "Last Month";
-};
-
-
-const timeslist=document.querySelectorAll("#daily,#weekly,#monthly");
-
+//putting eventlistner on the nodes andchanging the time according to what is clicked
 timeslist.forEach((time)=>{
     time.addEventListener("click",()=>{
         render(time.id);
@@ -49,4 +45,6 @@ timeslist.forEach((time)=>{
 
 });
 
+
+//finnaly calling the function
 loadData();
